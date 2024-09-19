69b8d267b210dcc4e640b6fb2a71ed3c76611b69
Kirill "Zkir <mekillot@yandex.ru>
2024-09-20 02:09:51 +0300

    final touch of the Alexander Column

--- ocga_samples/alexander_column_ocga.py
+++ ocga_samples/alexander_column_ocga.py
@@ -11,7 +11,7 @@ def checkRulesMy(ctx):
         ctx.material("stone")
         ctx.roof_colour("salmon")
         ctx.roof_material("stone")
-        ctx.split_z((('~6.7', 'angel_and_cross'), ('~3.8', 'column_top'), ('~1.25', 'platform'), ('~26.6', 'column'), ('~6.8', 'metal_base'), ('~2.0', 'stylobate'), ('1.5', 'steps')))
+        ctx.split_z((('~6.7', 'angel_and_cross'), ('~3.8', 'column_top'), ('~1.20', 'platform'), ('~26.6', 'column'), ('~6.8', 'metal_base'), ('~2.0', 'stylobate'), ('1.5', 'steps')))
 
     if ctx.getTag("building:part") == "angel_and_cross":
         ctx.colour("darkslategrey")
@@ -24,15 +24,24 @@ def checkRulesMy(ctx):
         ctx.scale("0.3", "0.3")
 
     if ctx.getTag("building:part") == "cross_bar":
-        ctx.scale("0.3", "2.5")
+        ctx.scale("2.5", "0.3")
 
     if ctx.getTag("building:part") == "column_top":
-        ctx.primitive_cylinder("1.7", "16")
-        ctx.create_roof("dome", "1.5")
         ctx.colour("darkslategrey")
         ctx.material("metal")
         ctx.roof_colour("darkslategrey")
         ctx.roof_material("metal")
+        ctx.primitive_cylinder("1.5", "16")
+        ctx.split_z((('~1', 'column_dome'), ('0.25', 'column_top_cornice'), ('~1', 'column_top_column')))
+
+    if ctx.getTag("building:part") == "column_dome":
+        ctx.create_roof("dome", "'0.95")
+
+    if ctx.getTag("building:part") == "column_top_cornice":
+        ctx.scale("'1.2", "'1.2")
+
+    if ctx.getTag("building:part") == "column_top_column":
+        ctx.nope()
 
     if ctx.getTag("building:part") == "platform":
         ctx.colour("darkslategrey")
@@ -52,7 +61,7 @@ def checkRulesMy(ctx):
         ctx.split_z((('0.3', 'column_base'), ('~1', 'base5'), ('~1', 'base4'), ('~0.5', 'base3'), ('~3.5', 'base2_with_bas_relief'), ('~2', 'base1')))
 
     if ctx.getTag("building:part") == "column_base":
-        ctx.primitive_cylinder("'1")
+        ctx.primitive_cylinder("'1", "16")
         ctx.scale("'0.8", "'0.8")
         ctx.comp_border("0.8", "column_base_part")
 
