2be80d87a508bb2c192ea7d25dcab150c07300a1
zkir <zkir@zkir.ru>
2024-10-25 00:11:46 +0300

    some adaptations for linux

---
+++ example.sh
@@ -0,0 +1,18 @@
+# example file to quickly convert and see 3d model
+# MODEL_NAME=rossi_pavilion
+MODEL_NAME=komsomolskaya_station
+# ===========  Step 1.  =================================================================================================
+# Apply ocga rules to building outline(s)
+python ocga_engine.py -i ocga_samples/$MODEL_NAME.osm -r ocga_samples/$MODEL_NAME.ocga -o ocga_output/$MODEL_NAME-rewrite.osm
+
+# ===========  Step 2.  =================================================================================================
+# Generate 3D model
+# we use osm2world, but blosm can be used as well
+# probably I am dumb, but I do not undersand how to use relative paths here
+
+(cd /home/zkir/osm2world  ; sh osm2world.sh -i /home/zkir/src/ocga/ocga_output/$MODEL_NAME-rewrite.osm -o /home/zkir/src/ocga/ocga_output/$MODEL_NAME.gltf )
+
+
+# ===========  Step 2.  =================================================================================================
+# activate 3d viewer. On Win10, it is built in.
+f3d ocga_output/$MODEL_NAME.gltf
