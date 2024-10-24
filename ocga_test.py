99d09997e2fa2d704de52a1d787ae1d78727efcf
zkir <zkir@zkir.ru>
2024-10-25 00:13:41 +0300

    some adaptations for linux

--- ocga_test.py
+++ ocga_test.py
@@ -4,22 +4,22 @@ from pathlib import Path
 from ocga_engine import ocga_process2
 
 print("ocga test")
-SAMPLES_DIR=".\\ocga_samples"
-OUTPUT_DIR=".\\ocga_output"
+SAMPLES_DIR="./ocga_samples"
+OUTPUT_DIR="./ocga_output"
 
 def test(rules_file1, input_file1):
-    
-    input_file= SAMPLES_DIR + "\\" + input_file1
-    output_file = OUTPUT_DIR + "\\" + Path(input_file1).stem + "-rewrite.osm"
-    reference_file =  SAMPLES_DIR + "\\" + Path(input_file1).stem + "-rewrite.osm"
-    
-    ocga_process2(input_file, output_file, SAMPLES_DIR +"\\"+ rules_file1, compiled_rules_file = OUTPUT_DIR + "\\" + Path(rules_file1).stem +'_ocga.py' )
-    
+
+    input_file= SAMPLES_DIR + "/" + input_file1
+    output_file = OUTPUT_DIR + "/" + Path(input_file1).stem + "-rewrite.osm"
+    reference_file =  SAMPLES_DIR + "/" + Path(input_file1).stem + "-rewrite.osm"
+
+    ocga_process2(input_file, output_file, SAMPLES_DIR +"/"+ rules_file1, compiled_rules_file = OUTPUT_DIR + "/" + Path(rules_file1).stem +'_ocga.py' )
+
     print()
-    
+
     result = filecmp.cmp(output_file, reference_file, shallow=False)
-    
-    return result     
+
+    return result
 
 #old py rules
 # note: we can process raw python files also, but all old files are converted to ocga!
