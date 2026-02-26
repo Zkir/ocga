78c78ed4d7a25e38843a575dfe80308115449694
Kirill B. aka Zkir <zkir@zkir.ru>
2026-02-27 02:22:49 +0300

    Automatic version in __init__.py

--- src/ocga/__init__.py
+++ src/ocga/__init__.py
@@ -1,2 +1,8 @@
+from importlib.metadata import version, PackageNotFoundError
 from .ocga_engine import ocga_process2
-__version__ = "0.1.0"
+
+try:
+    __version__ = version("ocga")
+except PackageNotFoundError:
+    # package is not installed, e.g. when running from local source
+    __version__ = "unknown"
